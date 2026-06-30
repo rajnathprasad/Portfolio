3621d6ecb3ea1a61c310060c8c5f6bca8dad2d0a
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-07-01 00:01:05 +0530

    foundation

---
+++ lib/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: "About",
+    href: "#about",
+  },
+  {
+    label: "Projects",
+    href: "#projects",
+  },
+  {
+    label: "Learn",
+    href: "/learn",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
---
+++ lib/site.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+  name: "Rajnath Prasad",
+
+  role: "Software Engineer • Educator • Creator",
+
+  description:
+    "Building software that solves real-world problems while teaching Computer Science.",
+
+  email: "prajnath704@gmail.com",
+
+  socials: {
+    github: "https://github.com/rajnathprasad",
+    linkedin: "https://www.linkedin.com/in/rajnathprasad/",
+    instagram: "https://www.instagram.com/rajnath_22",
+    youtube: "https://youtube.com/rajnathprasad",
+    leetcode: "https://leetcode.com/u/rajnathprasad/",
+  },
+};
